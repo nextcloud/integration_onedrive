5fd83f3d16a18198c60f153a93f60ac066e2132e
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-09-29 13:56:55 +0300

    added SPDX headers
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- templates/adminSettings.php
+++ templates/adminSettings.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 $appId = OCA\Onedrive\AppInfo\Application::APP_ID;
 \OCP\Util::addScript($appId, $appId . '-adminSettings');
 ?>
